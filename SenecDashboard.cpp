10954aaaa779b96dc80dce2789eaf5d430dab2f0
95arp_Dekstop <95.arpadkov@gmail.com>
2023-01-14 15:49:51 +0100

    Adds method to update labels

--- SenecDashboard.cpp
+++ SenecDashboard.cpp
@@ -17,8 +17,22 @@ void SenecDashboard::updateView()
 {
     PowerState state = client->getDashboardData();
 
-    std::string self_sufficency = state.getBatteryState();
+    //std::string self_sufficency = state.getBatteryState();
 
-    QString qdata = QString::fromStdString(self_sufficency);
-    ui.testLabel->setText(qdata);
+    QString self_sufficency = QString::fromStdString(state.getSelfSuffiency());
+    //ui.testLabel->setText(qdata);
+
+    QString battery_soc = QString::fromStdString(state.getBatterySOC());
+
+    QString generation = QString::fromStdString(state.getGeneration());
+    ui.generationLabel->setText(generation);
+
+    QString grid_state = QString::fromStdString(state.getGridState());
+    ui.gridLabel->setText(grid_state);
+
+    QString battery_state = QString::fromStdString(state.getBatteryState());
+    ui.batteryLabel->setText(battery_state);
+
+    QString usage = QString::fromStdString(state.getUsage());
+    ui.usageLabel->setText(usage);
 }
